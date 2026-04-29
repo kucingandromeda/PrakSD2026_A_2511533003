0e75b4d41862583166e97c0960351ad6e2faa6fa
araxnoid <araxnoid0@gmail.com>
2026-04-30 04:42:01 +0700

    menambahkan code tugas(AntrianLoket_2511533003 dan Queue_2511533003) dan laporan pratikum pekan 4, nim:2511533003

--- PrakSD2026_A_2511533003/src/pekan4_2511533003/AntrianLoket_2511533003.java
+++ PrakSD2026_A_2511533003/src/pekan4_2511533003/AntrianLoket_2511533003.java
@@ -1,3 +1,40 @@
 package pekan4_2511533003;
 
-public class AntrianLoket_2511533003 {}
+import java.util.Scanner;
+
+public class AntrianLoket_2511533003 {
+
+    public static void main(String[] args) {
+        Queue_2511533003 queue_3003 = new Queue_2511533003(100);
+
+        Scanner sc_3003 = new Scanner(System.in);
+        System.out.println("=== PROGRAM ANTRIAN LOKET ===");
+        while (true) {
+            System.out.println("1. Tambah Antrian");
+            System.out.println("2. Hapus Antrian");
+            System.out.println("3. Tampilkan Antrian");
+            System.out.println("4. Reverse");
+            System.out.println("5. keluar");
+
+            System.out.print("pilih menu: ");
+            int option_3003 = sc_3003.nextInt();
+            System.out.println("");
+            if (option_3003 == 1) {
+                queue_3003.enqueue_3003();
+            } else if (option_3003 == 2) {
+                queue_3003.dequeue_3003();
+            } else if (option_3003 == 3) {
+                queue_3003.display_3003();
+            } else if (option_3003 == 4) {
+                queue_3003.reverse();
+            } else if (option_3003 == 5) {
+                break;
+            } else {
+                System.out.println("Pilihan tidak tersedia pada opsi");
+            }
+            System.out.println("");
+        }
+
+        sc_3003.close();
+    }
+}
--- PrakSD2026_A_2511533003/src/pekan4_2511533003/Queue_2511533003.java
+++ PrakSD2026_A_2511533003/src/pekan4_2511533003/Queue_2511533003.java
@@ -1,5 +1,6 @@
 package pekan4_2511533003;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Queue_2511533003 {
@@ -8,24 +9,35 @@ public class Queue_2511533003 {
     int max_3003;
     String queue_3003[];
 
+    public Queue_2511533003(int capacity_3003) {
+        this.max_3003 = capacity_3003;
+        front_3003 = this.size_3003 = 0;
+        rear_3003 = capacity_3003 - 1;
+        queue_3003 = new String[this.max_3003];
+    }
+
     boolean isFull_3003(Queue_2511533003 queue_3003) {
-        return (queue_3003.size_3003 >= queue_3003.max_3003);
+        return (queue_3003.size_3003 == queue_3003.max_3003);
     }
 
     boolean isEmpty_3003(Queue_2511533003 queue_3003) {
         return (queue_3003.size_3003 == 0);
     }
 
-    void enqueue_3003(String item_3003) {
+    void enqueue_3003() {
         if (isFull_3003(this)) {
             System.out.println("penuh");
             return;
         }
 
+        Scanner sc_3003 = new Scanner(System.in);
+        System.out.print("Masukkan nama pelanggan: ");
+        String item_3003 = sc_3003.nextLine();
+
         this.rear_3003 = (this.rear_3003 + 1) % this.max_3003;
         this.queue_3003[this.rear_3003] = item_3003;
         this.size_3003 = this.size_3003 + 1;
-        System.out.println(item_3003 + " enqueue to queue");
+        System.out.println("Data berhasil ditambahkan ke antrian");
     }
 
     String dequeue_3003() {
@@ -37,30 +49,52 @@ public class Queue_2511533003 {
         String item_3003 = this.queue_3003[this.front_3003];
         this.front_3003 = (this.front_3003 + 1) % this.max_3003;
         this.size_3003 = this.size_3003 - 1;
+
+        System.out.println(item_3003 + " telah dilayani");
+
         return item_3003;
     }
 
     void display_3003() {
-        if (this.front_3003 == this.rear_3003) {
+        if (this.isEmpty_3003(this)) {
             System.out.println("\nAntrian Kosong\n");
             return;
         }
 
-        System.out.println("Isi Antrian: ");
-        for (int i_3003 = 0; i_3003 < this.size_3003; i_3003++) {
-            System.out.printf(i_3003 + ". ", queue_3003[i_3003]);
+        System.out.println("\nIsi antrian: \n");
+        for (
+            int i_3003 = this.front_3003;
+            i_3003 < this.front_3003 + this.size_3003;
+            i_3003++
+        ) {
+            System.out.println(" " + queue_3003[i_3003]);
         }
     }
 
     void reverse() {
+        if (isEmpty_3003(this)) {
+            System.out.println("Antrian Kosong");
+            return;
+        }
+
         Stack<String> s_3003 = new Stack<>();
 
-        for (int i_3003 = 0; i_3003 < this.size_3003; i_3003++) {
+        for (
+            int i_3003 = this.front_3003;
+            i_3003 < this.front_3003 + this.size_3003;
+            i_3003++
+        ) {
             s_3003.push(queue_3003[i_3003]);
         }
 
-        for (int i_3003 = 0; i_3003 < this.size_3003; i_3003++) {
+        for (
+            int i_3003 = this.front_3003;
+            i_3003 < this.front_3003 + this.size_3003;
+            i_3003++
+        ) {
             queue_3003[i_3003] = s_3003.pop();
         }
+
+        this.display_3003();
     }
 }
