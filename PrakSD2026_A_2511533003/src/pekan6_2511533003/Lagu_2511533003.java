06bb28e526eb4f580c9861088b060efaf229fd26
araxnoid <araxnoid0@gmail.com>
2026-05-14 05:39:02 +0700

    selesai membuat code Musik_2511533003

--- PrakSD2026_A_2511533003/src/pekan6_2511533003/Lagu_2511533003.java
+++ PrakSD2026_A_2511533003/src/pekan6_2511533003/Lagu_2511533003.java
@@ -2,48 +2,48 @@ package pekan6_2511533003;
 
 public class Lagu_2511533003 {
 
-    String judul;
-    String penyanyi;
-    Lagu_2511533003 next;
-    Lagu_2511533003 prev;
+    String judul_3003;
+    String penyanyi_3003;
+    Lagu_2511533003 next_3003;
+    Lagu_2511533003 prev_3003;
 
     // konstruktor
-    public Lagu_2511533003(String judul, String penyanyi) {
-        this.judul = judul;
-        this.penyanyi = penyanyi;
+    public Lagu_2511533003(String judul_3003, String penyanyi_3003) {
+        this.judul_3003 = judul_3003;
+        this.penyanyi_3003 = penyanyi_3003;
     }
 
     // getter
-    public String getJudul() {
-        return this.judul;
+    public String getJudul_3003() {
+        return this.judul_3003;
     }
 
-    public String getPenyanyi() {
-        return this.penyanyi;
+    public String getPenyanyi_3003() {
+        return this.penyanyi_3003;
     }
 
-    public Lagu_2511533003 getNext() {
-        return this.next;
+    public Lagu_2511533003 getNext_3003() {
+        return this.next_3003;
     }
 
-    public Lagu_2511533003 getPrev() {
-        return this.prev;
+    public Lagu_2511533003 getPrev_3003() {
+        return this.prev_3003;
     }
 
     // setter
-    public void getJudul(String judul) {
-        this.judul = judul;
+    public void getJudul_3003(String judu_3003l) {
+        this.judul_3003 = judul_3003;
     }
 
-    public void getPenyanyi(String penyanyi) {
-        this.penyanyi = penyanyi;
+    public void getPenyanyi_3003(String penyany_3003i) {
+        this.penyanyi_3003 = penyanyi_3003;
     }
 
-    public void getNext(Lagu_2511533003 next) {
-        this.next = next;
+    public void getNext_3003(Lagu_2511533003 nex_3003t) {
+        this.next_3003 = next_3003;
     }
 
-    public void getPrev(Lagu_2511533003 prev) {
-        this.prev = prev;
+    public void getPrev_3003(Lagu_2511533003 prev) {
+        this.prev_3003 = prev;
     }
 }
--- PrakSD2026_A_2511533003/src/pekan6_2511533003/Musik_2511533003.java
+++ PrakSD2026_A_2511533003/src/pekan6_2511533003/Musik_2511533003.java
@@ -4,8 +4,116 @@ import java.util.Scanner;
 
 public class Musik_2511533003 {
 
+    public static Lagu_2511533003 tambahLagu_3003(
+        Lagu_2511533003 head_3003,
+        Lagu_2511533003 tail_3003
+    ) {
+        Scanner sc_3003 = new Scanner(System.in);
+
+        System.out.print("judul: ");
+        String judul_3003 = sc_3003.nextLine();
+
+        System.out.print("Penyanyi: ");
+        String penyanyi_3003 = sc_3003.nextLine();
+
+        Lagu_2511533003 lagu_3003 = new Lagu_2511533003(
+            judul_3003,
+            penyanyi_3003
+        );
+
+        return lagu_3003;
+    }
+
+    public static void tampilMaju_3003(Lagu_2511533003 head_3003) {
+        Lagu_2511533003 curr_3003 = head_3003;
+        if (curr_3003 == null) {
+            System.out.println("Linked List Kosong!");
+        }
+
+        while (true) {
+            if (curr_3003 == null) {
+                break;
+            }
+
+            System.out.println(
+                "judul musik: " +
+                    curr_3003.getJudul_3003() +
+                    ", penyayi: " +
+                    curr_3003.getPenyanyi_3003()
+            );
+
+            curr_3003 = curr_3003.next_3003;
+        }
+    }
+
+    public static Lagu_2511533003 hapusLaguAwal_3003(
+        Lagu_2511533003 head_3003
+    ) {
+        if (head_3003 == null) {
+            System.out.println("Linked List Kosong!");
+            return null;
+        }
+
+        return head_3003.next_3003;
+    }
+
+    public static void tampilMundur_3003(Lagu_2511533003 tail_3003) {
+        Lagu_2511533003 curr_3003 = tail_3003;
+        if (curr_3003 == null) {
+            System.out.println("Linked List Kosong!");
+        }
+
+        while (true) {
+            if (curr_3003 == null) {
+                break;
+            }
+
+            System.out.println(
+                "judul musik: " +
+                    curr_3003.getJudul_3003() +
+                    ", penyayi: " +
+                    curr_3003.getPenyanyi_3003()
+            );
+
+            curr_3003 = curr_3003.prev_3003;
+        }
+    }
+
+    public static void cariLagu(Lagu_2511533003 head_3003) {
+        Lagu_2511533003 curr_3003 = head_3003;
+        if (curr_3003 == null) {
+            System.out.println("Linked List Kosong!");
+        }
+
+        Scanner sc_3003 = new Scanner(System.in);
+
+        System.out.print("masukkan judul yang ingin dicari: ");
+        String cariJudul_3003 = sc_3003.nextLine();
+
+        while (true) {
+            if (curr_3003 == null) {
+                System.out.println(
+                    "Lagu dengan judul " + cariJudul_3003 + " tidak ditemukan"
+                );
+                break;
+            }
+
+            if (curr_3003.getJudul_3003().equalsIgnoreCase(cariJudul_3003)) {
+                System.out.println(
+                    "Lagu dengan judul " + cariJudul_3003 + " ditemukan"
+                );
+                break;
+            }
+
+            curr_3003 = curr_3003.next_3003;
+        }
+    }
+
     public static void main(String[] args) {
         Scanner sc_3003 = new Scanner(System.in);
+        Lagu_2511533003 head_3003 = null;
+        Lagu_2511533003 tail_3003 = null;
+
         while (true) {
             System.out.println("\n=== Playlist Musik NIM: 2411531005 === ");
             System.out.println("1. Tambah Lagu ");
@@ -19,10 +127,31 @@ public class Musik_2511533003 {
             int option_3003 = sc_3003.nextInt();
 
             if (option_3003 == 1) {
+                Lagu_2511533003 lagu_3003 = tambahLagu_3003(
+                    head_3003,
+                    tail_3003
+                );
+                if (head_3003 == null && tail_3003 == null) {
+                    System.out.println("this run");
+                    head_3003 = lagu_3003;
+                    tail_3003 = lagu_3003;
+                } else {
+                    lagu_3003.prev_3003 = tail_3003;
+                    tail_3003.next_3003 = lagu_3003;
+                    tail_3003 = lagu_3003;
+                }
+
+                System.out.println("berhasil ditambahkan! ");
             } else if (option_3003 == 2) {
+                head_3003 = hapusLaguAwal_3003(head_3003);
+                head_3003.prev_3003 = null;
+                System.out.println("berhasil di hapus");
             } else if (option_3003 == 3) {
+                tampilMaju_3003(head_3003);
             } else if (option_3003 == 4) {
+                tampilMundur_3003(tail_3003);
             } else if (option_3003 == 5) {
+                cariLagu(head_3003);
             } else if (option_3003 == 6) {
                 break;
             } else {
@@ -30,6 +159,6 @@ public class Musik_2511533003 {
             }
         }
 
-        // sc_3003.close();
+        sc_3003.close();
     }
 }
